98de8df7332fd037a93a4d25b70027892204c6c8
Ashvin Antyah <a.antyah@gmail.com>
2024-05-02 19:56:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ const playSong = (id) => {
   }
   userData.currentSong = song;
   playButton.classList.add("playing");
+
   highlightCurrentSong();
   setPlayerDisplay();
   setPlayButtonAccessibleText();
@@ -105,35 +106,43 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
+
   playButton.classList.remove("playing");
   audio.pause();
 };
+
 const playNextSong = () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
     const nextSong = userData?.songs[currentSongIndex + 1];
+
     playSong(nextSong.id);
   }
 };
+
 const playPreviousSong = () => {
   if (userData?.currentSong === null) return;
   else {
     const currentSongIndex = getCurrentSongIndex();
     const previousSong = userData?.songs[currentSongIndex - 1];
+
     playSong(previousSong.id);
   }
 };
+
 const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
   userData.currentSong = null;
   userData.songCurrentTime = 0;
+
   renderSongs(userData?.songs);
   pauseSong();
   setPlayerDisplay();
   setPlayButtonAccessibleText();
 };
+
 const deleteSong = (id) => {
   if (userData?.currentSong?.id === id) {
     userData.currentSong = null;
@@ -176,16 +185,15 @@ const setPlayerDisplay = () => {
   const currentArtist = userData?.currentSong?.artist;
 
   playingSong.textContent = currentTitle ? currentTitle : "";
-
   songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
-
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
+
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
@@ -198,21 +206,17 @@ const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
       return `<li id="song-${song.id}" class="playlist-song">
-
   <button class="playlist-song-info" onclick="playSong(${song.id})" onclick="deleteSong(${song.id})"> 
   <span class="playlist-song-title">${song.title}</span> 
   <span class="playlist-song-artist">${song.artist}</span> 
   <span class="playlist-song-duration">${song.duration}</span> 
   </button>
-
   <button class="playlist-song-delete" aria-label="Delete ${song.title}">
 
   <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
-
-
-
   </button>
-  </li> `;
+  </li> 
+  `;
     })
     .join("");
 
@@ -269,24 +273,5 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+setPlayButtonAccessibleText();
 
-// const printGreeting= ()=>{
-//   console.log("Hello there!");
-// }
-// printGreeting()
-
-// const printMessage = (org) => {
-//   console.log(`${org} is awesome!`);
-// };
-// printMessage("freeCodeCamp")
-
-// const addTwoNumbers = (num1, num2) => num1+num2
-// console.log(addTwoNumbers(3,7));
-
-// const getTax = (price) => {
-//   const taxRate = 0.08;
-//   const tax = price * taxRate;
-//   return tax;
-// };
-// console.log(getTax(10));
-// console.log(getTax(100));
